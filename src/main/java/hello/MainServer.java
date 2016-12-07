9a726b51a4834985b7939e9de65bfc442b116c0f
Matheus Henrique <matheushgf.ferreira@gmail.com>
2016-12-06 22:18:53 -0200

    Update porta

--- src/main/java/hello/MainServer.java
+++ src/main/java/hello/MainServer.java
@@ -14,7 +14,7 @@ public class MainServer {
         if (process.environment().get("PORT") != null) {
             port = Integer.parseInt(process.environment().get("PORT"));
         } else {
-            port = 4567;
+            port = 8080;
         }
         port(port);
 
